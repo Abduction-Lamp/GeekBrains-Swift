cfd04b58ee02be15a1162d7167471ab4dfc8704d
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2020-11-28 01:40:06 +0300

    Removes the validation of the implemented functions

--- 2l_LesnykhVladimir.playground/Contents.swift
+++ 2l_LesnykhVladimir.playground/Contents.swift
@@ -11,25 +11,12 @@ func isEvenOrOddNumbers(number: Int) -> Bool {
     return ((number % 2) == 0) ? true : false
 }
 
-if isEvenOrOddNumbers(number: 7) {
-    print("Число четное")
-} else {
-    print("Число нечетное")
-}
-
 
 // Задание 2
 //
 func isDivisionBy3(number: Int) -> Bool {
     return ((number % 3) == 0) ? true : false
 }
-
-if isDivisionBy3(number: 15) {
-    print("Число делется на 3 без остаткa")
-} else {
-    print("Число на 3 без остатка не делится")
-}
-
 
 
 // Задание 3
