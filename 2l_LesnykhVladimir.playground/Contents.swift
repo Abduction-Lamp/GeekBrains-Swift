8c4c5a52122e923d4975e50d9fb96ed5fff75236
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2020-11-27 22:48:31 +0300

    Implements homework 2.1

---
+++ 2l_LesnykhVladimir.playground/Contents.swift
@@ -0,0 +1,19 @@
+//
+// Лесных Владимир
+//
+
+import PlaygroundSupport
+
+
+// Задание 1
+//
+func isEvenOrOddNumbers(namber: Int) {
+    if (namber % 2) == 0 {
+        print("Число \(namber) - четное")
+    } else {
+        print("Число \(namber) - нечетное")
+    }
+}
+
+isEvenOrOddNumbers(namber: 7)
+isEvenOrOddNumbers(namber: 4)
