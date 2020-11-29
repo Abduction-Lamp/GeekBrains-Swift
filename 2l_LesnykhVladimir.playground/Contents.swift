6c7b154fae81231e77e776154601b8a582bcff98
Vladimir Lesnykh <v.a.lesnykh@gmail.com>
2020-11-29 16:52:56 +0300

    Implements homework 2.4

--- 2l_LesnykhVladimir.playground/Contents.swift
+++ 2l_LesnykhVladimir.playground/Contents.swift
@@ -3,6 +3,7 @@
 //
 
 import PlaygroundSupport
+import Foundation
 
 
 // Задание 1
@@ -30,5 +31,48 @@ func getArray() -> [Int] {
 }
 
 var arrayOfNumber = getArray()
-print(arrayOfNumber)
+print("Исходный массив:\n \(arrayOfNumber)\n")
 
+
+// Задание 4
+//
+var i = 0
+while i < arrayOfNumber.count {
+    if isEvenOrOddNumbers(number: arrayOfNumber[i]) {
+        arrayOfNumber.remove(at: i)
+        continue
+    }
+    if isDivisionBy3(number: arrayOfNumber[i]) {
+        arrayOfNumber.remove(at: i)
+        continue
+    }
+    i += 1
+}
+
+print("Новый массив:\n\(arrayOfNumber)\n")
+
+
+// Второй способ
+//
+// Но на сколько я понял сложность это алгоритма выше
+// for value in arrayOfNumber   = O(n)
+// arrayOfNumber.remove         = O(n)
+// arrayOfNumber.firstIndex     = O(n)
+//                        ИТОГО = O(n^3)
+//
+// в первом способое получаеться  O(n^2)
+//
+arrayOfNumber = getArray()
+print("Исходный массив:\n \(arrayOfNumber)\n")
+
+for value in arrayOfNumber {
+    if isEvenOrOddNumbers(number: value) {
+        arrayOfNumber.remove(at: arrayOfNumber.firstIndex(of: value)!)
+        continue
+    }
+    if isDivisionBy3(number: value) {
+        arrayOfNumber.remove(at: arrayOfNumber.firstIndex(of: value)!)
+    }
+}
+
+print("Новый массив:\n\(arrayOfNumber)\n")
